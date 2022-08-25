61f76d6ff9a83f87c9062c153f5bc4610a40a193
Rselva1097 <99159175+Rselva1097@users.noreply.github.com>
2022-08-25 14:17:54 +0530

    bug fixed in authorization of headers

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -7,7 +7,7 @@ const auth = async (req, res, next) => {
   // check header
   const authHeader = req.headers.authorization
   console.log(authHeader)
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     throw new UnauthenticatedError('Authentication invalid')
   }
   const token = authHeader.split(' ')[1]
